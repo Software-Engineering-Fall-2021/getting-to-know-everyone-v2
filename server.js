b5675edb658933985de3f4266afa70190a04231c
Jocelyn Murray <murray.jocelyn.r216@gmail.com>
2021-10-08 20:59:24 -0500

    fixed port issue? maybe?

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = http.createServer(x);
 x.use(express.static(path.resolve(__dirname, './client')));
 
 console.log('Starting the server...')
-server.listen(process.env.PORT || 1337, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 443, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Server started, listening at:", addr.address + ":" + addr.port);
 });
